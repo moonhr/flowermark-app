6abdf57b651e9274a13791569efa7f04eeb07714
Choi YuJin <yjyj0774@gmail.com>
2025-04-13 21:17:04 +0900

    인원 선택 컴포넌트 생성

---
+++ features/createRoom/component/RoomCapacitySelect.tsx
@@ -0,0 +1,29 @@
+import { View, Text } from "react-native";
+import { Picker } from "@react-native-picker/picker";
+
+export default function RoomCapacitySelect({
+  value,
+  onChange,
+}: {
+  value: number | undefined;
+  onChange: (n: number) => void;
+}) {
+  const options = [2, 3, 4, 5];
+
+  return (
+    <View style={{ marginBottom: 16 }}>
+      <Text style={{ marginBottom: 8 }}>총 인원</Text>
+      <View style={{ borderWidth: 1, borderColor: "#ccc", borderRadius: 6 }}>
+        <Picker
+          selectedValue={value}
+          onValueChange={(itemValue: number) => onChange(itemValue)}
+        >
+          <Picker.Item label="인원 선택" value={null} />
+          {options.map((opt) => (
+            <Picker.Item key={opt} label={`${opt}명`} value={opt} />
+          ))}
+        </Picker>
+      </View>
+    </View>
+  );
+}
