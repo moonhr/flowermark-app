88b560eda55312a7f67e2f67ea0cbff9e48067e1
Choi YuJin <yjyj0774@gmail.com>
2025-04-16 16:28:56 +0900

    users 컬렉션에서 사용자 데이터 가져오기

---
+++ entities/user/model/fetchUsers.ts
@@ -0,0 +1,21 @@
+/**
+ * @yuxincxoi
+ * * 모든 사용자 데이터를 가져오는 유틸 함수입니다.
+ *
+ * @module fetchUsers
+ * @returns {Promise<User[]>} Firestore의 'users' 컬렉션에서 가져온 사용자 객체 배열
+ */
+
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "@/firebaseConfig";
+import { User } from "./types";
+
+// 'users' 컬렉션에서 사용자 데이터를 모두 가져와 User[] 형태로 반환
+export async function fetchUsers(): Promise<User[]> {
+  const querySnapshot = await getDocs(collection(db, "users"));
+
+  return querySnapshot.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+  })) as User[];
+}
