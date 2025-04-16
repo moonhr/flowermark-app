f790de4615e826a2e7558e41fcc4825ec0b5086a
Choi YuJin <yjyj0774@gmail.com>
2025-04-16 18:18:07 +0900

    user type 수정

--- entities/user/model/types.ts
+++ entities/user/model/types.ts
@@ -1,7 +1,9 @@
+import { Timestamp } from "firebase/firestore";
+
 export interface User {
-  id: string;
+  user_id: string;
   nickname: string;
   profile_image: string;
-  created_at: string;
-  updated_at: string;
+  created_at: Timestamp;
+  updated_at: Timestamp;
 }
