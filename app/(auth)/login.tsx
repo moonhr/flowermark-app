6b358fd99e506c89dde07b1fa825b9c71c338a7a
Choi YuJin <yjyj0774@gmail.com>
2025-04-17 20:12:44 +0900

    login screen 렌더링

---
+++ app/(auth)/login.tsx
@@ -0,0 +1,11 @@
+/**
+ * @yuxincxoi
+ * * 로그인 탭 화면의 엔트리 파일입니다.
+ *
+ * @module LoginTab
+ * @returns {JSX.Element} Login 화면을 렌더링하는 컴포넌트
+ */
+
+import LoginScreen from "@/features/auth/ui/LoginScreen";
+
+export default LoginScreen;
