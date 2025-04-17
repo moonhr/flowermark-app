d657548263ee86813bbdb9be10f1d08cf23085f0
Choi YuJin <yjyj0774@gmail.com>
2025-04-17 16:32:26 +0900

    주석 추가

--- entities/room/api/fetchRooms.ts
+++ entities/room/api/fetchRooms.ts
@@ -1,7 +1,16 @@
+/**
+ * @yuxincxoi
+ * * 모든 책방 데이터를 가져오는 유틸 함수입니다.
+ *
+ * @module fetchRooms
+ * @returns {Promise<Room[]>} Firestore의 'rooms' 컬렉션에서 가져온 책방 객체 배열
+ */
+
 import { collection, getDocs } from "firebase/firestore";
 import { db } from "@/firebaseConfig";
 import { Room } from "../model/types";
 
+// 'rooms' 컬렉션에서 사용자 데이터를 모두 가져와 Room[] 형태로 반환
 export async function fetchRooms(): Promise<Room[]> {
   const querySnapshot = await getDocs(collection(db, "rooms"));
   return querySnapshot.docs.map((doc) => doc.data() as Room);
