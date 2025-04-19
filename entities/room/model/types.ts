a70560c299163595402726d63cf093d2869c52e3
Choi YuJin <yjyj0774@gmail.com>
2025-04-19 15:07:11 +0900

    type 수정

--- entities/room/model/types.ts
+++ entities/room/model/types.ts
@@ -2,8 +2,7 @@ import { Timestamp } from "firebase/firestore";
 import { User } from "@/entities/user/model/types";
 
 export type Room = {
-  room_id: string;
-  creator_id: Timestamp;
+  creator_id: string;
   created_at: Timestamp;
   room_name: string;
   status: "시작전" | "진행중" | "완료";
