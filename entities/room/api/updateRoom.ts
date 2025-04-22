ad52f50e250717464d94ff24c7a3d2e99448e97b
Choi YuJin <yjyj0774@gmail.com>
2025-04-22 19:05:44 +0900

    db rooms update api 수정

--- entities/room/api/updateRoom.ts
+++ entities/room/api/updateRoom.ts
@@ -1,25 +1,31 @@
 /**
  * @yuxincxoi
- * * Firestore의 기존 방(Room)을 업데이트합니다.
+ * * Firestore의 기존 Room을 업데이트합니다.
  *
- * @param roomData 업데이트할 방의 데이터 (room_id 포함 필수)
+ * @param roomData 업데이트할 방의 데이터
  * @returns 업데이트된 문서의 참조 ID
  */
 
 import { db } from "@/firebaseConfig";
-import { doc, setDoc } from "firebase/firestore";
+import { doc, updateDoc } from "firebase/firestore";
 import { Room } from "@/entities/room/model/types";
+import { getRoomIdByCreator } from "./getRoomIdByCreator";
 
-export const updateRoom = async (
-  roomData: Partial<Room> & { room_id: string }
-) => {
+// todo : room_name -> creator_id로 변경하기
+export const updateRoom = async ({
+  exRoomName,
+  ...roomData
+}: Room & { exRoomName: string }) => {
   try {
-    const { room_id, ...data } = roomData;
+    // const room_id = await getRoomIdByCreator(roomData.creator_id);
+    const room_id = await getRoomIdByCreator(exRoomName);
+
+    if (!room_id) {
+      throw new Error("해당 이름의 방을 찾을 수 없습니다.");
+    }
 
     // 지정된 ID의 문서를 업데이트
-    await setDoc(doc(db, "rooms", room_id), data, { merge: true });
-
-    return room_id;
+    await updateDoc(doc(db, "rooms", room_id), roomData);
   } catch (error) {
     console.error("방 업데이트 중 오류 발생:", error);
     throw error;
