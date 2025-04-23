7d9dbb64605284af0ff872b12c6e5ee537b0f511
Choi YuJin <yjyj0774@gmail.com>
2025-04-23 15:55:15 +0900

    프로필 이미지 추가

---
+++ constants/defaultProfileImages.ts
@@ -0,0 +1,11 @@
+export const DEFAULT_PROFILE_IMAGES = [
+  require("../assets/profileImg/profile_1.png"),
+  require("../assets/profileImg/profile_2.png"),
+  require("../assets/profileImg/profile_3.png"),
+  require("../assets/profileImg/profile_4.png"),
+  require("../assets/profileImg/profile_5.png"),
+  require("../assets/profileImg/profile_6.png"),
+  require("../assets/profileImg/profile_7.png"),
+  require("../assets/profileImg/profile_8.png"),
+  require("../assets/profileImg/profile_9.png"),
+];
